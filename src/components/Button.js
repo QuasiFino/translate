b8987189ef6e94295e3abec433776a32df2635fb
wink-aajlak <aajlak.wink@gmail.com>
2021-11-27 10:43:10 +0530

    using default value and this.context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext; //variable name should not be changed
+
   render() {
+    console.log(this.context);
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
-      <button className="ui button primary">Submit</button>
+      <button className="ui button primary">{text}</button>
     );
   }
 }
 
+// Button.contextType = LanguageContext; //alt to static line
+
 export default Button;
+
+// static adds property directly to the class itself
